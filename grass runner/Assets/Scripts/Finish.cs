cd837df651613f426243bfa42378456f86c91836
Kamleshpc <124081981+Kamleshpc@users.noreply.github.com>
2023-11-02 23:51:04 +0530

    Complete the 2 Level

--- grass runner/Assets/Scripts/Finish.cs
+++ grass runner/Assets/Scripts/Finish.cs
@@ -1,27 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
     private AudioSource finishSound;
+
+    private bool levelCompleted = false;
+
     private void Start()
     {
         finishSound = GetComponent<AudioSource>();
     }
-    private void onTriggerEnter2D(Collider2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player" && !levelCompleted)
         {
             finishSound.Play();
-            CompleteLevel();
+            levelCompleted = true;
+            Invoke("CompleteLevel", 2f);
         }
     }
 
     private void CompleteLevel()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
-    
 }
