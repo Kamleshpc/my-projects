decea0f59e51af2bce92f3047cbc98727e088bda
Kamleshpc <124081981+Kamleshpc@users.noreply.github.com>
2023-11-07 22:55:14 +0530

    small changes

--- grass runner/Assets/Scripts/itemCollector.cs
+++ grass runner/Assets/Scripts/itemCollector.cs
@@ -6,17 +6,19 @@ using UnityEngine.UI;
 public class ItemCollector : MonoBehaviour
 {
     private int cherries = 0;
+
     [SerializeField] private Text cherriesText;
 
     [SerializeField] private AudioSource collectionSoundEffect;
-        private void OnTriggerEnter2D(Collider2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Cherry"))
         {
-            if (collision.gameObject.CompareTag("cherry"))
-            {
-                collectionSoundEffect.Play();
-                Destroy(collision.gameObject);
-                cherries++;
-                cherriesText.text = "Cherries:" + cherries;
-            }
+            collectionSoundEffect.Play();
+            Destroy(collision.gameObject);
+            cherries++;
+            cherriesText.text = "Cherries: " + cherries;
         }
+    }
 }
